9fccd9c51a56576f4ac691d620329a53f2fe6856
FarahAshraf <farah.ashraf@ictmisr.com>
2026-09-16 11:30:46 +0300

    edit the index.js

--- connections/db-connection.js
+++ connections/db-connection.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const url = process.env.MONGO_URL;
 
 async function connectDB() {
-    try {
-        await mongoose.connect(url, {
-            dbName: "learn-mongo"
-        });
-
-        console.log("Connected successfully to MongoDB");
-
-    } catch (error) {
-        console.error("MongoDB connection failed:", error.message);
+  try {
+    if (mongoose.connection.readyState === 1) {
+      return;
     }
+    await mongoose.connect(url, {
+      dbName: "learn-mongo",
+    });
+
+    console.log("Connected successfully to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    throw error;
+  }
 }
 
 module.exports = connectDB;
-
-
--- index.js
+++ index.js
@@ -1,55 +1,64 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('node:path');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("node:path");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./utils/swagger");
-const connectDB = require('./connections/db-connection');
-const courseRouter = require('./routes/courses.routes');
-const userRouter = require('./routes/users.routes');
-const httpStatusText = require('./utils/httpStatusText');
-
-
+const connectDB = require("./connections/db-connection");
+const courseRouter = require("./routes/courses.routes");
+const userRouter = require("./routes/users.routes");
+const httpStatusText = require("./utils/httpStatusText");
 
 const app = express();
 
-
-
 //Connect to MongoDB
-connectDB();
+// connectDB();
+app.use(async (req, res, next) => {
+  try {
+    await connectDB();
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 //enable cors for all origins
 app.use(cors());
-
 
 //middleware so the apis will read the body in a json format (use express.json or body-parser)
 app.use(express.json());
 
 //static route: Allow clients to access files stored in the uploads folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api/courses', courseRouter);
-app.use('/api/users', userRouter);
+app.use("/api/courses", courseRouter);
+app.use("/api/users", userRouter);
 
 // Swagger documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //for not found routes
 app.use((req, res) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Cannot ${req.method} ${req.originalUrl}`
-    });
+  res.status(404).json({
+    status: "fail",
+    message: `Cannot ${req.method} ${req.originalUrl}`,
+  });
 });
 
 //global middleware for errors
-app.use( (error, req, res, next) => {
-    res.status(error.statusCode || 500).json({ status: error.statusText || httpStatusText.ERROR, message: error.message, code: error.statusCode || 500, data: null });
-} )
+app.use((error, req, res, next) => {
+  res
+    .status(error.statusCode || 500)
+    .json({
+      status: error.statusText || httpStatusText.ERROR,
+      message: error.message,
+      code: error.statusCode || 500,
+      data: null,
+    });
+});
 
+// app.listen(process.env.PORT, (req, res) => {
+//   console.log("listening on port 5000");
+// });
 
-
-app.listen(process.env.PORT, (req,res) => {
-    console.log('listening on port 5000');
-})
+module.exports = app;
